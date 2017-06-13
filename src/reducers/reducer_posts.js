08101389276f48b761fbcfa2bc53a50b33701060
Bien Pham <pham_bv@hotmail.com>
2017-06-13 12:22:04 -0400

    added delete post actions and ui elements

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -2,14 +2,24 @@
  * Created by Bien on 2017-06-12.
  */
 import _ from 'lodash';
-import { FETCH_POSTS } from '../actions';
+import { FETCH_POSTS, FETCH_POST, DELETE_POST} from '../actions';
 
 
 export default function(state = {}, action) {
         switch (action.type) {
-        case FETCH_POSTS:
-            return _.map(action.payload.data, 'id');
-        default:
-            return state;
+            case DELETE_POST:
+                return _.omit(state, action.payload);
+                return _.reject(state, post => post.id === action.payload);
+            case FETCH_POST:
+                // const post = action.payload.data;
+                // const newState = {...state }
+                // newState[post.id] = post;
+                // return newState;
+
+                return { ...state, [action.payload.data.id]: action.payload.data };
+            case FETCH_POSTS:
+                return _.map(action.payload.data, 'id');
+            default:
+                return state;
     }
 }
